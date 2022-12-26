3dd00c588b2f7bea71bf0e4e1df830e56f343bcc
Supachai Yuenyao <Supachai.Y@WynnSoft-Solution.com>
2022-12-26 13:29:18 +0700

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using JwtTokenTutorial.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+namespace JwtTokenTutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private List<User> appUsers = new List<User>
+        {
+            new User { FullName = "Vaibhav Bhapkar", UserName = "admin", Password = "1234", UserRole = "Admin" },
+            new User { FullName = "Test User", UserName = "user", Password = "1234", UserRole = "User" }
+        };
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody]User login)
+        {
+            IActionResult response = Unauthorized();
+            User user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJWTToken(user);
+                response = Ok(new
+                {
+                    token = tokenString,
+                    userDetails = user,
+                });
+            }
+            return response;
+        }
+
+        [HttpGet]
+        [Route("GetDataToken")]
+        [Authorize]
+        public IActionResult GetTokenData(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]);
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                // var accountId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+                return Ok(new {
+                    data = jwtToken,
+                });
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(501, new {
+                    message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("GetUserData")]
+        [Authorize(Policy = Policies.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("This is a response from user method");
+        }
+        
+        [HttpGet]
+        [Route("GetAdminData")]
+        [Authorize(Policy = Policies.Admin)]
+        public IActionResult GetAdminData()
+        {
+            return Ok("This is a response from Admin method");
+        }
+
+        User AuthenticateUser(User loginCredentials)
+        {
+            User user = appUsers.SingleOrDefault(x => x.UserName == loginCredentials.UserName && x.Password == loginCredentials.Password);
+            return user;
+        }
+        string GenerateJWTToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim("fullName", userInfo.FullName.ToString()),
+                new Claim("role",userInfo.UserRole),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtTokenTutorial.Models;
+using JwtTokenTutorial.Controllers;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace JwtTokenTutorial
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+             services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                                {
+                                    // policy.WithOrigins("http://example.com", "http://www.contoso.com")
+                                    //         .AllowAnyHeader()
+                                    //         .AllowAnyMethod();
+                                });
+            });
+            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            services.AddAuthorization(config =>
+            {
+                config.AddPolicy(Policies.Admin, Policies.AdminPolicy());
+                config.AddPolicy(Policies.User, Policies.UserPolicy());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
